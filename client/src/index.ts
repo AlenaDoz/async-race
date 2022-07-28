9e361b916f1dc1d92b83d9831230d1a98575e18c
AlenaDoz <alena.abunassar1@mail.ru>
2022-07-28 15:03:17 +0300

    feat: winners is generated by ts

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,5 @@
 import './style.css';
 import GaragePage from './components/pages/garage';
 new GaragePage().drawPage();
+import WinnersPage from './components/pages/winners';
+new WinnersPage().drawPage();
